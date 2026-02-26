0f55de095ac8f7fe889b551f583496450563baaa
Luma <65837288+Luma-exe@users.noreply.github.com>
2026-02-26 11:45:06 +1100

    chore: bump version to v2.3.0

--- src/main.cpp
+++ src/main.cpp
@@ -464,7 +464,7 @@ int main() {
     if (port_env) port = std::atoi(port_env);
 
     // Build version string for banner
-    string ver_line = "    Universal Media Toolkit v2.2.0";
+    string ver_line = "    Universal Media Toolkit v2.3.0";
 
     if (g_git_commit != "unknown") {
         ver_line += "  (" + g_git_branch + "@" + g_git_commit + ")";
